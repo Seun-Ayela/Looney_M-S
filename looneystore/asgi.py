6dc871d4894a757869a10b853c8a519b27b5ddfe
unknown <jspacific777@gmail.com>
2023-09-09 14:01:47 +0100

    renamed startproject

--- looneystore/asgi.py
+++ looneystore/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for drfecommerce project.
+ASGI config for looneystore project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drfecommerce.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'looneystore.settings')
 
 application = get_asgi_application()
--- looneystore/wsgi.py
+++ looneystore/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for drfecommerce project.
+WSGI config for looneystore project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drfecommerce.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'looneystore.settings')
 
 application = get_wsgi_application()
